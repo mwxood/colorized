43b8f16d9a1e9a67a4a3616dc0ca1c7f048db649
Mihail <mwxood@users.noreply.github.com>
2022-06-27 19:05:49 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 import Header from './components/header/header';
 import Color from './components/color/color';
@@ -6,25 +6,61 @@ import randomColor from "randomcolor";
 
 function App() {
   const [color, setColor] = useState(['#3eed17', '#db3b6d', '#61f4b0', '#d33298']);
+  // const [isLocked, setLock] = useState(false);
+  const ref = useRef();
 
   const colorsHandler = () => {
-       const colors = [];
+    const colors = [];
 
-        for(let i = 0; i < 4; i++) {
-            let color = randomColor();
-            colors.push(color)
+     for(let i = 0; i < 4; i++) {
+         let color = randomColor();
+         colors.push(color)
+     }
+     setColor([...colors])
+}
+
+useEffect( () => {
+ 
+})
+
+// const lockHandler = (data) => {
+//   setLock(!isLocked);
+//   ref.current = data.target;
+// }
+
+const store = [
+  {
+      color: [
+        {
+          id: 1,
+          color: color[0]
+        },
+        {
+          id: 2,
+          color: color[1]
+        },
+        {
+          id: 3,
+          color: color[2]
+        },
+        {
+          id: 4,
+          color: color[3]
         }
-
-        setColor([...colors])
+      ]
   }
+]
+  
 
-
-
+  useEffect(() => {
+ 
+  }); 
+  
 
   return (
     <div className="App full-height ">
       <Header changeColors={colorsHandler} />
-      <Color colors={color}  />
+      <Color colorsArray={store}   />
     </div>
   );
 }
--- src/components/color/color.js
+++ src/components/color/color.js
@@ -1,14 +1,25 @@
+import React, { useState } from 'react';
 import './color.css';
 
 
-const color = (props) => {
 
- const colors = props.colors.map(item => {
+
+const Color = (props) => {
+    const [isLocked, setLock] = useState(false);
+    
+    const locked = () => {
+        setLock(!isLocked)
+    }
+
+
+// console.log(props)
+ const colors = props.colorsArray[0].color.map((item, index) => {
+  
     return(
-            <div className='col-md-3 remove-padding' key={Math.random(Math.floor() * 10)}>
-                <div className='color full-height d-flex align-items-center justify-content-center' style={{backgroundColor: `${item}`}}>
-                    <div className='lock-holder'>
-                        <i className="fa-solid fa-lock"></i>
+            <div  className='col-md-3 remove-padding' key={item.id}>
+                <div className='color full-height d-flex align-items-center justify-content-center' style={{backgroundColor: `${item.color}`}}>
+                    <div id={props.id} onClick={locked} className='lock-holder'>
+                        <i className={isLocked ? 'icon icon fa-solid fa-lock' : 'icon fa-solid fa-lock-open'}></i>
                     </div>
                 </div>
             </div>
@@ -17,19 +28,10 @@ const color = (props) => {
  })
 
     return(
-  
         <div className='row full-height'>
-            {/* <div className='col-md-3 remove-padding'>
-                <div className='color full-height d-flex align-items-center justify-content-center color-red'>
-                    <div className='lock-holder'>
-                        <i className="fa-solid fa-lock"></i>
-                    </div>
-                </div>
-            </div> */}
-
             {colors}
         </div>
     );
 }
 
-export default color;
+export default Color;
